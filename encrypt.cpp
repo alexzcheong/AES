e3f35e67f17ab189ef1e8fd94dc2fab9cfbcde09
alexzcheong <113654786+alexzcheong@users.noreply.github.com>
2024-04-14 17:33:45 -0700

    Update encrypt.cpp

--- encrypt.cpp
+++ encrypt.cpp
@@ -5,7 +5,7 @@
 #include <random>
 #include <inttypes.h>
 typedef std::mt19937 MyRNG;
-uint32_t seed_val = 0x19EE448F;//random seed value
+uint32_t seed_val = 0x19EE448F;
 MyRNG rng;
 void initialize()
 {
@@ -20,7 +20,7 @@ uint_fast32_t* splitCipher(uint_fast32_t* in) {
     for(uint_fast32_t quarter : ciphersplit){
         for(int i = 0; i < 32; i++){
             int bit = gen(rng);
-            //std::cout << bit;//for debugging
+            std::cout << bit;
             quarter = quarter | bit; //last bit of quarter to rng bit
             quarter = (quarter << 1) | (quarter >> 31); // rot left by 1
         }
@@ -39,19 +39,22 @@ uint_fast32_t *makeRoundKey(uint_fast32_t *key) {
 }
 uint_fast32_t *rotate(uint_fast32_t *key) {
     uint_fast32_t temp = key[3];
-    key[3] = key[1];
-    key[1] = temp;
+    key[3] = key[0];
+    key[0] = temp;
     return key;
 }
 
 uint_fast32_t *mix(uint_fast32_t *key) {
-    //TODO
+    //wtf is a hill cypher
     return nullptr;
 }
 
 uint_fast32_t *substitute(uint_fast32_t *key) {
-    //TODO
-    return nullptr;
+    uint_fast8_t *byte = (uint_fast8_t *)key;//pragye
+    for(int i = 0; i < 4; i++){
+        byte[i] = lookup(byte[i]);
+    }
+    return key;
 }
 
 uint_fast8_t lookup(uint_fast8_t &byte) {
@@ -68,7 +71,7 @@ uint_fast32_t *addRoundKey(const uint_fast32_t *key, uint_fast32_t *text) {
     return text;
 }
 
-uint_fast32_t *encrypt(uint_fast32_t *key, uint_fast32_t *text) {
+uint_fast32_t *encrypt(const uint_fast32_t *key, uint_fast32_t *text) {
     uint_fast32_t copy[4];//do not change original key so we can send to recipient
     uint_fast32_t* next = copy;//for pointer reasons lol
     for(int i = 0; i < 4; i++){
@@ -78,5 +81,5 @@ uint_fast32_t *encrypt(uint_fast32_t *key, uint_fast32_t *text) {
         text = addRoundKey(next, text);
         next = makeRoundKey(next);
     }
-    return nullptr;
+    return text;
 }
