b0df6f14eeae193718705c4f2589a8e1d2274253
alexzcheong <113654786+alexzcheong@users.noreply.github.com>
2024-04-14 19:37:03 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@
 #include "encrypt.h"
 int main() {
     uint_fast32_t cipher[4] = {0};
-    uint_fast8_t bytearr[4][4];
-    uint_fast32_t round_keys[11] = {0};
 
     uint_fast32_t *head = splitCipher(cipher);
     for(int i = 0; i < 4; i++){
