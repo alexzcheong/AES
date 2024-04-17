b80deb59e0c56a160018dadcb909f5856027ecb5
alexzcheong <113654786+alexzcheong@users.noreply.github.com>
2024-04-16 21:10:46 -0700

    Update encrypt.cpp

--- encrypt.cpp
+++ encrypt.cpp
@@ -30,10 +30,19 @@ uint_fast32_t* splitCipher(uint_fast32_t* in) {
     return in;
 }
 
-uint_fast32_t *makeRoundKey(uint_fast32_t *key) {
+uint_fast32_t *makeRoundKey(uint_fast32_t *key, int round) {
+    uint_fast32_t temp[4];
+    uint_fast32_t *copy = temp;//copy key for xor to generate new key later
+    for(int i = 0; i < 4; i++){
+        copy[i] = key[i];
+    }
     substitute(key);
     rotate(key);
-    mix(key);
+    rcon(key, round);
+    key[0] = copy[0] ^ key[3];
+    key[1] = key[0] ^ copy[1];
+    key[2] = key[1] ^ copy[2];
+    key[3] = key[2] ^ copy[3];
     return key;
 }
 uint_fast32_t *rotate(uint_fast32_t *key) {
@@ -45,8 +54,16 @@ uint_fast32_t *rotate(uint_fast32_t *key) {
     return key;
 }
 
-uint_fast32_t *mix(uint_fast32_t *key) {
-    //wtf is a hill cypher
+uint_fast32_t* rcon(uint_fast32_t *key, int round) {
+    uint_fast8_t *byte = (uint_fast8_t *)key;//pragye
+    for(int i = 0; i < 16; i++){
+        byte[i] = byte[i] ^ num::roundTable[round - 1];//a constant derived from current round is XOR'd with the bytes in the word
+    }
+    return key;
+}
+
+uint_fast32_t *mix(uint_fast32_t *state, const uint_fast32_t *key) {
+    //matrix addition [state] + [key] (as addition is invertible while multiplication is not)
     return nullptr;
 }
 
@@ -72,15 +89,31 @@ uint_fast32_t *addRoundKey(const uint_fast32_t *key, uint_fast32_t *text) {
     return text;
 }
 
+uint_fast32_t *processText(uint_fast32_t *state, uint_fast32_t *key) {
+    substitute(state);
+    rotate(state);
+    mix(state, key);
+    return state;
+}
+
 uint_fast32_t *encrypt(const uint_fast32_t *key, uint_fast32_t *text) {
     uint_fast32_t copy[4];//do not change original key so we can send to recipient
     uint_fast32_t* next = copy;//for pointer reasons lol
     for(int i = 0; i < 4; i++){
-        next[i] = key[i];
+        next[i] = key[i];//deep copy key
     }
-    for (int i = 0; i < 11; i++){
-        text = addRoundKey(next, text);
-        next = makeRoundKey(next);
+
+    uint_fast32_t* roundKeys[11];//put key into list
+    roundKeys[0] = copy;
+    addRoundKey(copy, text);//0th round for AES 128
+    for (int i = 1; i < 11; i++){
+        roundKeys[i] = makeRoundKey(roundKeys[i - 1], i);//make key for current round from previous key
+        processText(text, roundKeys[i]);//do encryption things on text
+        addRoundKey(roundKeys[i], text);//xor current round key with text
     }
     return text;
 }
+
+
+
+
